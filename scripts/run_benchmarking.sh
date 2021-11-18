2ac01f3308087f55b6c1c965a57ec5a9d543d44a
Benjamin Cathelineau <bencathelif@gmail.com>
2021-11-18 16:19:15 +0100

    push modifications

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,7 +4,7 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
+for i in $(seq 1000000 1000000 10000000) ; do
     for rep in `seq 1 5`; do
         echo "Size: $i" >> $OUTPUT_FILE;
         ./src/parallelQuicksort $i >> $OUTPUT_FILE;
